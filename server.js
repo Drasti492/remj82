74fb08276bf6199c9361c211b53b4f9ecf6e64f6
malocox <collo8432@gmail.com>
2025-11-10 15:27:05 +0300

    Backend combined and ready for Render deployment

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
   "http://127.0.0.1:5500",
-  "https://remote-pro-jobs.vercel.app"
+  "https://remote-pro-jobs.vercel.app",
+   "https://remoteprojobs.site" 
 ];
 
 app.use(
