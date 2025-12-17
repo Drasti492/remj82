b370a9f1faf652595daffa178b11332bab6798b3
drasti492 <modes7267@gmail.com>
2025-12-17 12:23:02 +0300

    Backend combined and ready for Render deployment

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const paymentController = require("../controllers/paymentController");
 const authMiddleware = require("../middleware/auth");
 
+router.get("/test-payhero", async (req, res) => {
+  try {
+    const axios = require("axios");
+
+    const r = await axios.get("https://backend.payhero.co.ke", {
+      timeout: 10000
+    });
+
+    res.json({ ok: true });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+
 // STK Push
 router.post("/stk-push", authMiddleware, paymentController.stkPush);
 
